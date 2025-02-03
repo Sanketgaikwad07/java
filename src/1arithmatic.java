281eaf769bdaa798762a872bc7e8ba0341e7bb6c
SanketG-7 <sanketgaikwad1820@gmail.com>
2025-02-03 22:13:01 +0530

    init commit

--- src/1arithmatic.java
+++ src/1arithmatic.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class arithmatic {
+ class arithmatic {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("please enter your first number: ");
