ebbf393a33a940e5a53cb0931b7883db0d947abc
Sanket Gaikwad <sanketgaikwad1820@gmail.com>
2025-03-19 07:17:36 +0530

    Create absolute.java

---
+++ slip/absolute.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class absolute {
+
+
+     public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         System.out.println("Enter your number:");
+         int num= sc.nextInt();
+         int result=num>=0? num:-num;
+         System.out.println("your absolute value is:"+result);
+
+     }
+
+
+
+}
