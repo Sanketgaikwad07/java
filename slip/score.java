4ebaa4f4cfbafaed4044d299d61aabf03bb75c3f
Sanket Gaikwad <sanketgaikwad1820@gmail.com>
2025-03-19 07:18:25 +0530

    Create score.java

---
+++ slip/score.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class StudentScore {
+     public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         System.out.println("Enter your marks:");
+         int marks= sc.nextInt();
+         String catgory=marks>80?"high":(marks>50?"maderate":"low");
+         System.out.println("your catgory is: "+catgory);
+     }
+
+
+
+
+
+}
