94a1bbfd31df152a1102b8024a5cf11b4d79792d
Sanket Gaikwad <sanketgaikwad1820@gmail.com>
2025-04-06 23:26:29 +0530

    Create cycle.java

---
+++ slip/cycle.java
@@ -0,0 +1,10 @@
+public class CycleExample {
+    public static void main(String[] args) {
+        // Example of a while loop cycling from 1 to 5
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Cycle " + i);
+            i++;
+        }
+    }
+}
