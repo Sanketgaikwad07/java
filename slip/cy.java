5054df9e57eadc7c5eeef64f00343e77932295b3
Sanket Gaikwad <sanketgaikwad1820@gmail.com>
2025-04-06 23:26:53 +0530

    Create cy.java

---
+++ slip/cy.java
@@ -0,0 +1,8 @@
+public class CycleExample {
+    public static void main(String[] args) {
+        // Example of a for loop cycling from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Cycle " + i);
+        }
+    }
+}
