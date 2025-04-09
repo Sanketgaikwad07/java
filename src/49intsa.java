d6359de76d14943e5334a3b215a878f7a172422d
SanketG-7 <sanketgaikwad1820@gmail.com>
2025-04-10 01:16:19 +0530

    init commit

---
+++ src/49intsa.java
@@ -0,0 +1,34 @@
+class intsa {
+
+    public static void main(String[] args) {
+        String jsonStr = """
+                        {
+                        "name":"%s",
+                
+                        "age":%d,
+                
+                    }
+                """.formatted("sanket", 35);
+        System.out.println(jsonStr);
+
+
+    }
+}
+
+//
+//@FunctionalInterface
+//interface click
+//{
+//void click();
+//}
+//
+//    public static void buttoncilck(click click) {
+//    click.click();
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        System.out.println();}
+//
+//}
