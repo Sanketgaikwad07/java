ed90b486e3070a0824b0c9501a47147338e61893
SanketG-7 <sanketgaikwad1820@gmail.com>
2025-04-15 10:32:51 +0530

    init commit

---
+++ src/45ForLoop.java
@@ -0,0 +1,21 @@
+class ForLoop {
+
+    public static void main(String[] args) {
+        int i = 1;
+        //int j=0;
+//         while (i <= 100) {
+//             System.out.print(i + " ");
+//             i++;
+//
+//         }
+        System.out.println();
+//         for (int j = 1; j <= 100; j++) {
+//             System.out.print(j + " ");
+
+
+        for (int j = 1; j <= 100; j += 2) {
+            System.out.print(j + " ");
+
+        }
+    }
+}
