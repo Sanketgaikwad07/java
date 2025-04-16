8092048892bd871dd034d9a743abe3e1559e3d0f
SanketG-7 <sanketgaikwad1820@gmail.com>
2025-04-16 19:15:31 +0530

    init commit

---
+++ src/52flower.java
@@ -0,0 +1,15 @@
+ class flower {
+
+
+        public static void main(String[] args) {
+            System.out.println("     @@@     ");
+            System.out.println("   @     @   ");
+            System.out.println("  @       @  ");
+            System.out.println("   @     @   ");
+            System.out.println("     @@@     ");
+            System.out.println("       |     ");
+            System.out.println("       |     ");
+            System.out.println("       |     ");
+        }
+    }
+
