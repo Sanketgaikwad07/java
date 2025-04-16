4068022451add65f3ed0a26f69be7f261166db01
SanketG-7 <sanketgaikwad1820@gmail.com>
2025-04-16 20:25:24 +0530

    init commit

--- src/2calculate.java
+++ src/2calculate.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 
- class calculate {
-    public static void main(String[] args) {
+//import java.util.Scanner;
+//
+// class calculate {
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//
+//    }
+//}
+class calculate{
+    public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-
+        System.out.println("Good morning");
+        int first=sc.nextInt();
+        System.out.println("Hey Hi"+first);
     }
 }
