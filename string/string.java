373f31913eb3a97542cbcaae7742f7ff8010b68f
Sanket Gaikwad <178196375+SanketG-7@users.noreply.github.com>
2025-04-18 20:49:41 +0530

    Create string.java

---
+++ string/string.java
@@ -0,0 +1,8 @@
+public class stringManipulicaon {
+    public static void main(String[] args) {
+        String firstname = "Sanket";
+        String lastName = "Gaikwad";
+        String Name = firstname.concat(" ").concat(lastName);
+        System.out.println(Name.toUpperCase());
+    }
+}
