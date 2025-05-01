780570ef7b3784bf74f9cfb70fe8e89f10f295d2
SanketG-7 <sanketgaikwad1820@gmail.com>
2025-05-01 23:27:56 +0530

    init comit

---
+++ src/72For.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class For {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int sum=sc.nextInt();
+
+        for(int i = 0; i<sum; i++){
+            for(int j=0;j<sum;j++){
+                System.out.println("Saket");
+            }
+        }
+
+    }
+}
