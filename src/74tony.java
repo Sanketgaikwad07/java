a157dc11a7c6c157a62c6bc6a02a9ae3b4093f2e
SanketG-7 <sanketgaikwad1820@gmail.com>
2025-05-01 23:29:36 +0530

    init commit

---
+++ src/74tony.java
@@ -0,0 +1,13 @@
+class tony {
+    public static void main(String[] args) {
+
+        String name = "Sanket";
+//substring(beg index,end index)
+
+        String fullname = name.substring(4, name.length());
+        System.out.println(fullname);
+
+        //System.out.println("my name is :"+name);
+
+    }
+}
