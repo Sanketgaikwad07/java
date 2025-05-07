734de6d3c7b2de2da64bca1d0e3e1191c70bf378
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-05-07 20:57:27 +0530

    Create float.java

---
+++ string/float.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+ class floating {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("ew are doing multiplication");
+        System.out.println("please enter the first decimal number");
+         double first =sc.nextDouble();
+        System.out.println("please enter the second number");
+        double second=sc.nextDouble();
+
+        System.out.println("\n result is: " + first+second);
+
+    }
+}
