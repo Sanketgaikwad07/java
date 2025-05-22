1f2acefbd0061b9ececf69024ed36b2cfac0eae1
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-05-22 22:20:05 +0530

    Create matrix.java

---
+++ matrix.java
@@ -0,0 +1,46 @@
+public class MatrixMultiplication {
+
+    public static void main(String[] args) {
+        int[][] A = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int[][] B = {
+            {7, 8},
+            {9, 10},
+            {11, 12}
+        };
+
+        // Number of rows of A, columns of B
+        int rowsA = A.length;
+        int colsA = A[0].length;
+        int rowsB = B.length;
+        int colsB = B[0].length;
+
+        if (colsA != rowsB) {
+            System.out.println("Matrix multiplication is not possible.");
+            return;
+        }
+
+        int[][] result = new int[rowsA][colsB];
+
+        // Matrix multiplication
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        // Print result
+        System.out.println("Result of Matrix Multiplication:");
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
