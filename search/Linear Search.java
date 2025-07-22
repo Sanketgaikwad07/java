3dc65ee1e18e10d746bafe1540b34ce65b86fc26
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-07-22 08:47:05 +0530

    Create Linear Search.java

---
+++ search/Linear Search.java
@@ -0,0 +1,17 @@
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;  // Found at index i
+            }
+        }
+        return -1;  // Not found
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+        int target = 30;
+        int result = linearSearch(nums, target);
+        System.out.println("Element found at index: " + result);
+    }
+}
