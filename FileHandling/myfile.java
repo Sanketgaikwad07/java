4fd2adfd24451cb08c468bca35ca61d125ccb323
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-08-03 11:29:47 +0530

    Create myfile.java

---
+++ FileHandling/myfile.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+
+public class error {
+    public static void main(String[] args) {
+        try {
+            File myfil = new File("homey.js");
+            if (myfil.createNewFile()) {
+                myfil.wait(120);
+                System.out.println("myfille" + myfil);
+            } else {
+                System.out.println("not create ");
+            }
+        } catch (IOException e) {
+            System.out.println("not done");
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
