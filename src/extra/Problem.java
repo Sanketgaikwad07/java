bef070363cddea9d731efc8058d6f487ada163f7
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2025-10-18 09:13:09 +0530

    Add main method to Problem class

--- src/extra/Problem.java
+++ src/extra/Problem.java
@@ -1,6 +1,7 @@
 package extra;
 
 public class Problem {
+    
     public static void main(String[] args) {
         for (int i = 1; i > 0; i++) {
             MultiProblem mything = new MultiProblem(i);
