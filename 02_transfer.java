856776bf37843e83d15d015ebad6b86949880822
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2026-02-17 22:11:26 +0530

    Update 02_transfer.java

--- 02_transfer.java
+++ 02_transfer.java
@@ -14,6 +14,7 @@ public class StudentRecordDisplay extends JFrame {
 
         
         JPanel panel = new JPanel(new GridLayout(4, 2));
+        
 
         panel.add(new JLabel("Roll No:"));
         txtRNo = new JTextField();
