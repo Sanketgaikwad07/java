4f5ad760ca90e90566969e594236dcba62a885dc
Sanket Gaikwad <178196375+Sanketgaikwad07@users.noreply.github.com>
2026-04-04 23:04:49 +0530

    Update Query.java

--- Query.java
+++ Query.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseInsert {
+    
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/your_database"; // Database URL
         String user = "your_username"; // Database username
